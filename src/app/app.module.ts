b0868a80b5156214d856402b575e532c68219cae
Rocío Poza <rocio.poza.tejeda@gmail.com>
2020-02-21 20:04:22 +0100

    create project arquitecture and add different components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,20 @@ import { AppComponent } from './app.component';
 import { AngularFireModule } from '@angular/fire';
 import { environment } from '../environments/environment';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { IndexComponent } from './containers/index/index.component';
+import { CarrouselComponent } from './components/carrousel/carrousel.component';
+import { MapComponent } from './components/map/map.component';
+import { RsvpComponent } from './components/rsvp/rsvp.component';
 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    CarrouselComponent,
+    MapComponent,
+    RsvpComponent
   ],
   imports: [
     BrowserModule,
