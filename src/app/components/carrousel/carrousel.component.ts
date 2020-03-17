427e6880b3a56c373cffebdb6e60082444c369c9
Rocío Poza <rocio.poza.tejeda@gmail.com>
2020-03-17 21:16:02 +0100

    add carrousel pagination logic

--- src/app/components/carrousel/carrousel.component.ts
+++ src/app/components/carrousel/carrousel.component.ts
@@ -1,4 +1,14 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {
+  Component,
+  Input,
+  OnInit,
+  OnChanges,
+  OnDestroy,
+  ElementRef,
+  Renderer2,
+  ViewChild,
+  SimpleChanges
+} from '@angular/core';
 
 
 @Component({
@@ -6,14 +16,46 @@ import {Component, Input, OnInit} from '@angular/core';
   templateUrl: './carrousel.component.html',
   styleUrls: ['./carrousel.component.scss']
 })
-export class CarrouselComponent implements OnInit {
+export class CarrouselComponent implements OnInit, OnChanges, OnDestroy {
   @Input() pictureUrls: string[];
+  @ViewChild('carrousel') carrousel: ElementRef;
+
+  private pictureInterval: number;
+  private activeImage: number;
+  private imageWidth: number;
 
   constructor() {
     this.pictureUrls = [];
+    this.activeImage = 0;
+    this.imageWidth = 0;
   }
 
   ngOnInit(): void {
+    this.pictureInterval = window.setInterval(this.changePicture.bind(this), 1000);
   }
 
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.pictureUrls.currentValue.length) {
+      setTimeout(() => {
+        this.imageWidth = this.carrousel.nativeElement.children[0].offsetWidth;
+        console.log('this.carrousel.nativeElement.children[0]', this.carrousel.nativeElement.children[0].offsetWidth)
+        console.log('this.carrousel.nativeElement.children[0]', this.carrousel.nativeElement.children[0].children[0].offsetWidth)
+        console.log(this.imageWidth);
+      }, 100);
+    }
+  }
+
+  ngOnDestroy(): void {
+    window.clearInterval(this.pictureInterval);
+  }
+
+  changePicture() {
+    if (this.activeImage < this.pictureUrls.length - 1) {
+      this.activeImage = this.activeImage + 1;
+    } else {
+      this.activeImage = 0;
+    }
+
+    this.carrousel.nativeElement.scrollLeft = this.activeImage * this.imageWidth;
+  }
 }
