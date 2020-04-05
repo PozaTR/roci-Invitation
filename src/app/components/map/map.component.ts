4a543ce8d3cf3eca46a93d3bfbedf5a72b037660
Rocío Poza <rocio.poza.tejeda@gmail.com>
2020-04-05 13:43:22 +0200

    add google maps configuration and render map with real coordenates

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-map, [app-map]',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.scss']
 })
-export class MapComponent implements OnInit {
+export class MapComponent implements OnInit, AfterViewInit {
+  @ViewChild('mapContainer', {static: false}) gmap: ElementRef;
+  map: google.maps.Map;
+  private lat: number;
+  private lng: number;
+  private coordinates: google.maps.LatLng;
+  private mapOptions: google.maps.MapOptions;
 
-  constructor() { }
+  constructor() {
+    this.lat = 40.448318;
+    this.lng = -3.648065;
+    this.coordinates = new google.maps.LatLng(this.lat, this.lng);
+    this.mapOptions = {
+      center: this.coordinates,
+      zoom: 15,
+    };
+  }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    this.mapInitializer();
+  }
+
+  mapInitializer() {
+    this.map = new google.maps.Map(this.gmap.nativeElement, this.mapOptions);
   }
 
 }
