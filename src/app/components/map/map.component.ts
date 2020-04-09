e22ee20f049aa484ed0accee639e971fed15899a
Rocío Poza <rocio.poza.tejeda@gmail.com>
2020-04-09 21:28:36 +0200

    #4- Adapt zoom to include both markers

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -8,12 +8,14 @@ import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angula
 export class MapComponent implements OnInit, AfterViewInit {
   @ViewChild('mapContainer', {static: false}) gmap: ElementRef;
   map: google.maps.Map;
+  geocoder: google.maps.Geocoder;
   private lat: number;
   private lng: number;
   private coordinates: google.maps.LatLng;
   private mapOptions: google.maps.MapOptions;
   private marker: google.maps.Marker;
-  private userAddress: string;
+  private searchMarker: google.maps.Marker;
+  public userAddress: string;
   private searchAddress: string;
 
   constructor() {
@@ -44,11 +46,30 @@ export class MapComponent implements OnInit, AfterViewInit {
   mapInitializer() {
     this.map = new google.maps.Map(this.gmap.nativeElement, this.mapOptions);
     this.marker.setMap(this.map);
+    this.geocoder = new google.maps.Geocoder();
   }
 
   getAddress() {
     this.searchAddress = this.userAddress;
     console.log(this.searchAddress);
+    this.geocoder.geocode( { address: this.searchAddress}, (results, status) => {
+      if (status === 'OK') {
+       // this.map.setCenter(results[0].geometry.location);
+        this.searchMarker = new google.maps.Marker({
+          map: this.map,
+          position: results[0].geometry.location,
+          animation: google.maps.Animation.DROP
+        });
+        const bounds =  new google.maps.LatLngBounds();
+        const loc = new google.maps.LatLng(this.searchMarker.getPosition().lat(), this.searchMarker.getPosition().lng());
+        bounds.extend(loc);
+        bounds.extend(this.coordinates);
+        this.map.fitBounds(bounds);
+        this.map.panToBounds(bounds);
+      } else {
+        alert('Geocode was not successful for the following reason: ' + status);
+      }
+    });
   }
 
 }
