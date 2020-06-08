2e3ac61aeb105dd2dc0d718e2afeabcbf8c675b5
Rocío Poza <rocio.poza.tejeda@gmail.com>
2020-06-08 17:26:18 +0200

    change texts

---
+++ src/app/pipes/object-keys.pipe.ts
@@ -0,0 +1,11 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'objectKeys'
+})
+export class ObjectKeysPipe implements PipeTransform {
+
+  transform(value: object): string[] {
+    return Object.keys(value);
+  }
+}
