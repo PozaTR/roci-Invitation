8efeb7428cceed28c6de5b268bfbbd702034106f
Rocío Poza <rocio.poza.tejeda@gmail.com>
2020-06-20 15:46:18 +0200

    add guest service

---
+++ src/app/services/guest.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/database';
+import 'firebase/database';
+import { Observable } from 'rxjs';
+import { Guest } from '../../interfaces/guest';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GuestService {
+  private database: AngularFireDatabase;
+
+  constructor(database: AngularFireDatabase) {
+    this.database = database;
+  }
+
+  getGuest(guestId: string): Observable<Guest> {
+    console.log(`guests/${guestId}`)
+    return this.database.object<Guest>(`guests/${guestId}`).valueChanges();
+  }
+}
